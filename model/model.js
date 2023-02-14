7ed2b904446c3c53a4a8a9e5b1b68225f26545a5
Pintoo Singh <pintoo@consultadapt.com>
2023-02-14 18:04:24 +0530

    image

---
+++ model/model.js
@@ -0,0 +1,17 @@
+const mongooses = require("../db/db.js");
+ 
+var mongoose = require('mongoose');
+ 
+var imageSchema = new mongoose.Schema({
+    name: String,
+    desc: String,
+    img:
+    {
+        data: Buffer,
+        contentType: String
+    }
+});
+ 
+//Image is a model which has a schema imageSchema
+ 
+module.exports = new mongoose.model('Image', imageSchema);
