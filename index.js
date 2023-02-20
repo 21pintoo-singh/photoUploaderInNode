dc64b12f01e9fa5b925aefdb8b3fd46cb5bb6a76
Pintoo Singh <pintoo@consultadapt.com>
2023-02-20 10:46:03 +0530

    un usal file commanted

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ var createError = require('http-errors');
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-// const path = require('path');
+const path = require('path');
 const fs = require("fs");
 const multer = require("multer");
 const mongoose = require("mongoose");
 var imageModel = require('./model/model.js');
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-// var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 mongoose.set(
     'strictQuery', false);
 
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded(
 
 
 
-// app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine","ejs");
  
 // SET STORAGE
@@ -46,12 +46,12 @@ var storage = multer.diskStorage({
  
 var upload = multer({ storage: storage })
  
-// app.get("/",(req,res)=>{
-//     res.render("index");
-// })
+app.get("/",(req,res)=>{
+    res.render("index");
+})
  
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.post("/uploadphoto",upload.single('myImage'),(req,res)=>{
     var img = fs.readFileSync(req.file.path);
     var encode_img = img.toString('base64');
@@ -70,32 +70,29 @@ app.post("/uploadphoto",upload.single('myImage'),(req,res)=>{
         }
     })
 })
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(bodyParser.urlencoded(
-//   { extended:true }
-// ))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // error handler
-// app.use(function(err, req, res, next) {
-//     // set locals, only providing error in development
-//     res.locals.message = err.message;
-//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
   
-//     // render the error page
-//     res.status(err.status || 500);
-//     res.render('error');
-//   });
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 //Code to start server
 app.listen(3000,function () {
       console.log("Server Started at PORT 3000");
 })
 
-// app.use(function(req, res, next) {
-//     next(createError(404));
-//   });
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
 
 module.exports = {conn,app};
